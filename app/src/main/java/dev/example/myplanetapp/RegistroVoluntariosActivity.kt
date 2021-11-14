20afd5701a46c29a2ecf815cf573874c0d4505f8
EDER <16100911@ue.edu.pe>
2021-11-14 13:39:42 -0500

    Cambio Eder.1 - Creación de 2 activities main

---
+++ app/src/main/java/dev/example/myplanetapp/RegistroVoluntariosActivity.kt
@@ -0,0 +1,11 @@
+package dev.example.myplanetapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistroVoluntariosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_voluntarios)
+    }
+}
