9cd7475ff991cd7d85a4bc420f46a14b8c3a4830
Paolo Maldonado <jorpal_10_20@outlook.com>
2021-11-27 02:12:59 -0500

    Agregado: FirebaseRecycler View

--- app/src/main/java/dev/example/myplanetapp/adapters/RecyclerViewAllEventsAdapter.kt
+++ app/src/main/java/dev/example/myplanetapp/adapters/RecyclerViewAllEventsAdapter.kt
@@ -6,10 +6,15 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.storage.FirebaseStorage
 import dev.example.myplanetapp.R
 import dev.example.myplanetapp.model.VolunteeringEvent
 
-class RecyclerViewAllEventsAdapter(private var lstEvt: List<VolunteeringEvent>) : RecyclerView.Adapter<RecyclerViewAllEventsAdapter.ViewHolder>()
+class RecyclerViewAllEventsAdapter(options: FirestoreRecyclerOptions<VolunteeringEvent>) :
+    FirestoreRecyclerAdapter<VolunteeringEvent, RecyclerViewAllEventsAdapter.ViewHolder>(options)
 {
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
@@ -20,24 +25,21 @@ class RecyclerViewAllEventsAdapter(private var lstEvt: List<VolunteeringEvent>) 
         val eventSpots: TextView = itemView.findViewById(R.id.eventSpots)
     }
 
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): RecyclerViewAllEventsAdapter.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return ViewHolder(layoutInflater.inflate(R.layout.event_card, parent, false))
     }
 
-    override fun onBindViewHolder(holder: RecyclerViewAllEventsAdapter.ViewHolder, position: Int) {
-        val itemRow = lstEvt[position]
-        holder.eventTitle.text = itemRow.title
-        holder.eventTime.text = itemRow.time
-        holder.eventSpots.text = itemRow.spots.toString()
-        holder.eventPicture.setImageResource(itemRow.picture)
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int, model: VolunteeringEvent) {
+        holder.eventTitle.text = model.title
+        holder.eventTime.text = model.time
+        holder.eventSpots.text = model.spots.toString()
 
-    override fun getItemCount(): Int {
-        return lstEvt.size
+        val storageReference = FirebaseStorage.getInstance().reference.child(model.picture.toString())
+
+        GlideApp.with(holder.itemView)
+            .load(storageReference)
+            .into(holder.eventPicture)
     }
 
 }
