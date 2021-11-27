4282ba258b5b325c2486d4f1530805296673b1d4
EDER <16100911@ue.edu.pe>
2021-11-27 03:18:49 -0500

    Cambio Eder.3 - fragments y conexión con Loginactivity

--- app/src/main/java/dev/example/myplanetapp/RegistroVoluntario/FragmentDatos2.kt
+++ app/src/main/java/dev/example/myplanetapp/RegistroVoluntario/FragmentDatos2.kt
@@ -14,6 +14,7 @@ import dev.example.myplanetapp.R
 class FragmentDatos2 : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
